c115334da995897989db5e95ac16c3e53cd00950
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-24 11:47:25 +0100

    add configuration. close #2.

--- UserService/src/main/java/de/tekup/user/api/UserServiceApplication.java
+++ UserService/src/main/java/de/tekup/user/api/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
