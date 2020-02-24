7c8d6774ec3e5ad68729024b081053e696e269f6
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-24 11:44:32 +0100

    Init UserService

---
+++ UserService/src/test/java/de/tekup/user/api/UserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tekup.user.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
