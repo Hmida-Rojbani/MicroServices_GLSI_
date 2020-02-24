c91d17cc4697d36c9e5f713ce431a374839d2bfd
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-24 12:00:26 +0100

    Init GateWay server

---
+++ GateWayService/src/main/java/de/tekup/proxy/service/GateWayServiceApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.proxy.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayServiceApplication.class, args);
+	}
+
+}
