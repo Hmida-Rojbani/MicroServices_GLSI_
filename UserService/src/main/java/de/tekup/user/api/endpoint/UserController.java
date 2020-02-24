d9e7d56cabfeb2ea0457510c437369987e1ea80a
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-24 12:27:16 +0100

    Update UserService to be scalable

--- UserService/src/main/java/de/tekup/user/api/endpoint/UserController.java
+++ UserService/src/main/java/de/tekup/user/api/endpoint/UserController.java
@@ -1,5 +1,7 @@
 package de.tekup.user.api.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping
 	public String welcome() {
-		return "Welcome to User MicroService";
+		return "Welcome to User MicroService on the port :"+ env.getProperty("local.server.port");
 	}
 
 }
